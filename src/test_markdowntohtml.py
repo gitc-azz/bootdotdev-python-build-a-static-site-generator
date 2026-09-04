94d9fb1ce9d0779b8b44215d83bcd2ad4388acf8
gitc-azz <graphical.it.consulting@proton.me>
2026-09-04 20:57:54 +0200

    paragraph with code italic and bold parts passing

--- src/test_markdowntohtml.py
+++ src/test_markdowntohtml.py
@@ -4,8 +4,21 @@ from markdowntohtml import *
 
 class TestMarkdownToHtml(unittest.TestCase):
     def test_markdown_to_html_node(self):
-        md = "I a just a plain paragraph in one line"
-        self.assertEqual(markdown_to_html_node(md).to_html(),
-                         "<div><p>I a just a plain paragraph in one line</p></div>")
+        tcs = [
+                ("I a just a plain paragraph in one line",
+                 "<div><p>I a just a plain paragraph in one line</p></div>"),
+                ("I am just a **bold** paragraph in one line",
+                 "<div><p>I am just a <b>bold</b> paragraph in one line</p></div>"),
+                (
+"""I am
+a multi
+line with _italic_
+and some `one line code`
+""", "<div><p>I am\na multi\nline with <i>italic</i>\nand some <code>one line code</code></p></div>"
+)
+                ]
+        for tc in tcs:
+            self.assertEqual(markdown_to_html_node(tc[0]).to_html(),
+                             tc[1])
 
 
